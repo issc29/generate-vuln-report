4199df9a0befb8fe46f605ec7efccf444730adc7
Isaac Cohen <issc29@gmail.com>
2020-07-02 16:17:29 -0400

    Update index.js

--- index.js
+++ index.js
@@ -4,43 +4,47 @@ const github = require('@actions/github');
 
 // most @actions toolkit packages have async methods
 async function run() {
+  try {
+    const myToken = core.getInput('myToken');
+    const octokit = github.getOctokit(myToken)
+    const context = github.context;
 
-  const myToken = core.getInput('myToken');
-  const octokit = github.getOctokit(myToken)
-  const context = github.context;
+    console.log("Running Queries")
+    const { data: alerts } = await octokit.codeScanning.listAlertsForRepo({
+      ...context.repo
+    });
 
-  console.log("Running Queries")
-  const { data: alerts } = await octokit.codeScanning.listAlertsForRepo({
-    ...context.repo
-  });
+    console.log(alerts)
 
-  console.log(alerts)
-
-  const query =
-    `query ($org: String! $repo: String! $cursor: String){
-      repository(owner: $org name: $repo) {
-        name
-        vulnerabilityAlerts(first: 100 after: $cursor) {
-          pageInfo {
-            hasNextPage
-            endCursor
-          }
-          totalCount
-          nodes {
-            id
-            securityAdvisory {
-              ghsaId,
-              severity
+    const query =
+      `query ($org: String! $repo: String! $cursor: String){
+        repository(owner: $org name: $repo) {
+          name
+          vulnerabilityAlerts(first: 100 after: $cursor) {
+            pageInfo {
+              hasNextPage
+              endCursor
+            }
+            totalCount
+            nodes {
+              id
+              securityAdvisory {
+                ghsaId,
+                severity
+              }
             }
           }
         }
-      }
-    }`
-    
-  const variables = {...context.repo}
-  const result = await octokit.graphql(query, variables);
-  console.log(result)
-  console.log("ran queries")
+      }`
+      
+    const variables = {...context.repo}
+    const result = await octokit.graphql(query, variables);
+    console.log(result)
+    console.log("ran queries")
+  } catch (error) {
+    core.debug(error);
+    core.setFailed(error.message);
+  }
 }
 
 run()
