dca4f6855956d8cd609c2a9ecd4ef5efbbb1979d
Isaac Cohen <issc29@gmail.com>
2020-07-06 12:01:31 -0400

    update

--- dist/index.js
+++ dist/index.js
@@ -402,6 +402,13 @@ async function run() {
 
     console.log(alerts)
 
+    var alertCount = {'error': 0,'warning': 0,'note': 0}
+    alerts.forEach(alert => {
+      alertCount[alert.rule_severity] = alertCount[alert.rule_severity] + 1
+    });
+
+    console.log(alertCount)
+
     const query =
       `query ($org: String! $repo: String! $cursor: String){
         repository(owner: $org name: $repo) {
@@ -423,7 +430,6 @@ async function run() {
         }
       }`
       
-    const variables = {...context.repo}
     let pagination = null
     var ossVulns = []
     try {
@@ -432,7 +438,7 @@ async function run() {
         const getVulnResult = await octokit.graphql({query, org: context.repo.owner, repo: context.repo.repo, cursor: pagination })
         hasNextPage = getVulnResult.repository.vulnerabilityAlerts.pageInfo.hasNextPage
         const vulns = getVulnResult.repository.vulnerabilityAlerts.nodes
-        ossVulns.push(vulns)
+        Array.prototype.push.apply(ossVulns, vulns)
         if (hasNextPage) {
           pagination = getVulnResult.repository.vulnerabilityAlerts.pageInfo.endCursor
         }
